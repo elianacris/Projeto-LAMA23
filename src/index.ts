e046c030f80cfa79def949260f3246e766bb2d0f
ellianacris <ellianacris@hotmail.com>
2022-03-21 09:12:28 -0300

    alteraçoes

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,7 @@ import {AddressInfo} from "net";
 import express from "express";
 import { userRouter } from "./routes/userRouter";
 import { bandRouter } from "./routes/bandRouter";
-
 dotenv.config();
-
 const app = express();
 
 app.use(express.json());
@@ -13,7 +11,7 @@ app.use(express.json());
 app.use("/user", userRouter);
 app.use("/band", bandRouter);
 
-const server = app.listen(3003, () => {
+const server = app.listen(3000, () => {
     if (server) {
       const address = server.address() as AddressInfo;
       console.log(`Servidor rodando em http://localhost:${address.port}`);
